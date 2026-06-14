d868cc34d609e8a9828b62cace71b4fadd16bb18
Артем <amostaev45@gmail.com>
2026-06-14 18:37:24 +0300

    Убрал HttpClient из using

--- ServerStatusChecker/HttpService.cs
+++ ServerStatusChecker/HttpService.cs
@@ -4,13 +4,12 @@ namespace ServerStatusChecker
 {
     public static class HttpService
     {
+        private static readonly HttpClient httpClient = new HttpClient();
+
         public static async Task<bool> CheckStatusAsync(string url)
         {
-            using (var httpClient = new HttpClient())
-            {
-                var response = await httpClient.GetAsync(url);
-                return response.StatusCode == HttpStatusCode.OK ? true : false;
-            }
+            var response = await httpClient.GetAsync(url);
+            return response.StatusCode == HttpStatusCode.OK ? true : false;
         }
     }
 }
